c3f563cca6e520d2b2adfc3eff7e1a61df7c3d3e
Vijay Barman <vbarman116@gmail.com>
2025-02-04 23:22:05 +0530

    Updated the project

--- public/script.js
+++ public/script.js
@@ -1,12 +1,13 @@
 // Select all elements with the class 'button'
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll(".button");
 
 // Add event listeners to each button
-buttons.forEach(button => {
-  button.addEventListener('click', function(event) {
+buttons.forEach((button) => {
+  button.addEventListener("click", function (event) {
     // Get the id of the clicked button
     const colorId = event.target.id;
-    
+
+    // Change the background color of the page
     document.body.style.backgroundColor = colorId;
   });
 });
